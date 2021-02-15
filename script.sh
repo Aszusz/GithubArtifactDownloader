b56dcf16e276702cce28d81bd6d83a650704557a
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2021-02-15 20:32:39 +0100

    Made script more robust

--- script.sh
+++ script.sh
@@ -4,38 +4,79 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CREDENTIALS=$(<credentials.json)
-USER=$(echo "$CREDENTIALS" | jq -r ".user")
-TOKEN=$(echo "$CREDENTIALS" | jq -r ".token")
-CREDS="${USER}:${TOKEN}"
+CREDENTIALS_FILE=$"credentials.json" 	# Required
+METADATA_FILE=$"metadata.json"			# Optional
 
-echo "$CREDS"
-
-CACHED_ARTIFACT_DATA=$(<cached-artifact-data.json) || true
-
-LATEST_ARTIFACT_DATA=$(curl \
-	-H "Accept: application/vnd.github.v3+json" \
-	-u "$CREDS" \
-	https://api.github.com/repos/prominic/SwitchBoard-Internal-Use-Only/actions/artifacts \
-	| jq ".artifacts[0]")
+main() {
+	CREDENTIALS=$(load_credentials)
+	echo "Loaded credentials."
 	
-CACHED_ID=$(echo "$CACHED_ARTIFACT_DATA" | jq ".id")
-LATEST_ID=$(echo "$LATEST_ARTIFACT_DATA" | jq ".id")
-
-CACHED_DATE=$(echo "$CACHED_ARTIFACT_DATA" | jq ".updated_at")
-LATEST_DATE=$(echo "$LATEST_ARTIFACT_DATA" | jq ".updated_at")
-
-if [ "$CACHED_ID" != "$LATEST_ID" ] || [ "$CACHED_DATE" != "$LATEST_DATE" ]; then
-	DOWNLOAD_URL=$(echo "$LATEST_ARTIFACT_DATA" | jq -r ".archive_download_url")
-	LOCATION_URL=$(curl \
-	-H "Accept: application/vnd.github.v3+json" \
-	-u "$CREDS" \
-	-sIXGET \
-	"$DOWNLOAD_URL" \
-	| grep location: \
-	| awk '{print $2}')
+	CACHED_METADATA=$(load_metadata)
+	if [[ -z "$CACHED_METADATA" ]]; then
+		echo "No cached metadata."
+	else
+		echo "Loaded metadata form cache."
+	fi
 	
-	wget -nv "$LOCATION_URL" -O artifact.zip
+	LATEST_METADATA=$(download_metadata "$CREDENTIALS")
 	
-    echo "$LATEST_ARTIFACT_DATA" > cached-artifact-data.json
-fi
+	if [[ "$CACHED_METADATA" == "$LATEST_METADATA" ]]; then
+		echo "Cached metadata is up to date, nothing to do."
+	else		
+		echo "Cached metadata is old or empty, downloading new artifact."
+		LOCATION_URL=$(get_location_url "$LATEST_METADATA" "$CREDENTIALS")
+		download_artifact "$LOCATION_URL"	
+		echo "Artifact downloaded."
+		
+		echo "$LATEST_METADATA" > "$METADATA_FILE"
+		echo "Cached latest metadata on disk."
+	fi	
+}
+
+load_credentials () {
+	CONTENT=$(cat "$CREDENTIALS_FILE")
+
+	USER=$(echo "$CONTENT" | jq -r ".user")
+	if [[ "$USER" == null ]]; then
+		echo "Error: No user." >&2
+		exit 1
+	fi
+
+	TOKEN=$(echo "$CONTENT" | jq -r ".token")
+	if [[ "$TOKEN" == null ]]; then
+		echo "Error: No token." >&2
+		exit 1
+	fi
+
+	echo "$USER":"$TOKEN"
+}
+
+load_metadata() {
+	CONTENT=$(cat "$METADATA_FILE" || true)
+	echo "$CONTENT"
+}
+
+download_metadata () { 
+	HEADER=$"Accept: application/vnd.github.v3+json"
+	CREDENTIALS="$1"
+	URL="https://api.github.com/repos/prominic/SwitchBoard-Internal-Use-Only/actions/artifacts"	
+	METADATA=$(curl -H "$HEADER" -u "$CREDENTIALS" "$URL" -s | jq ".artifacts[0]")
+	echo "$METADATA"
+}
+
+get_location_url () {
+	METADATA="$1"
+	CREDENTIALS="$2"	
+	HEADER=$"Accept: application/vnd.github.v3+json"	
+	DOWNLOAD_URL=$(echo "$METADATA" | jq -r ".archive_download_url")
+	LOCATION_URL=$(curl -H "$HEADER" -u "$CREDENTIALS" -sIXGET "$DOWNLOAD_URL" \
+	| grep location: | awk '{print $2}')
+	echo "$LOCATION_URL"
+}
+
+download_artifact () {
+	LOCATION_URL="$1"
+	wget -q --show-progress --progress=bar:force:noscroll "$LOCATION_URL" -O artifact.zip
+}
+
+main "$@"; exit
